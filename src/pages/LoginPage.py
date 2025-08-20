dc4004002c8082dbda91b3f34498ca7d466af2a8
Ailen Fuentes <ailengfuentes@gmail.com>
2025-08-20 20:11:58 -0300

    varias cosas feas

--- src/pages/LoginPage.py
+++ src/pages/LoginPage.py
@@ -10,26 +10,23 @@ class LoginPage(BasePage):
 
     login_data= "login.json"
 
-def __init__(self, driver):
-        """Inicializa la clase LoginPage con una instancia de WebDriver."""
-        super().__init__(driver)
-        self.driver = driver
-
-def navigate_to_login_page(self, url):
-    get_url = self.get_data(self.login_data)
-    url_value = get_url["url"]
-    self.navigate_to(url_value)  # Usa el parámetro URL
-
-def enter_username(self, username):
-    get_username = self.get_data(self.login_data)
-    username_value = get_username["username"]
-    self.input_text(self.username_input, username_value)
 
 
-def enter_password(self, password):
-    get_password = self.get_data(self.login_data)
-    password_value = get_password["password"]
-    self.input_text(self.password_input, password_value)
+    def navigate_to_login_page(self):
+        get_url = self.get_data(self.login_data)
+        url_value = get_url["url"]
+        self.navigate_to(url_value)  # Usa el parámetro URL
 
-def click_login_button(self):
-    self.click_element(self.login_button)
+    def enter_username(self):
+        get_username = self.get_data(self.login_data)
+        username_value = get_username["username"]
+        self.input_text(self.username, username_value)
+
+
+    def enter_password(self):
+        get_password = self.get_data(self.login_data)
+        password_value = get_password["password"]
+        self.input_text(self.password, password_value)
+
+    def click_login_button(self):
+        self.click_element(self.login_button)
--- src/test/test_login_page.py
+++ src/test/test_login_page.py
@@ -4,20 +4,24 @@ from src.pages.LoginPage import LoginPage
 
 
 class TestLoginPage:
-    @pytest.fixture(scope="class")
-    def setup(self):
-        """Inicializa el WebDriver y la página de login."""
-        self.driver = BasePage.initialize_ChromeDriver_headless()
-        self.login_page = LoginPage(self.driver)
-        yield
-        self.driver.quit()
 
-    def test_login(self, setup):
-        """Prueba el proceso de inicio de sesión."""
-        self.login_page.navigate_to_login_page("https://www.saucedemo.com/")
-        self.login_page.enter_username("standard_user")
-        self.login_page.enter_password("secret_sauce")
-        self.login_page.click_login_button()
+    @pytest.fixture
+    def driver(self, driver):
+        # setup → antes del test
+        driver = BasePage.initialize_ChromeDriver_headless()
+        driver.max_window()
+        yield driver
+        # teardown → despues del test
+        driver.quit()
 
-        # Aquí podrías agregar aserciones para verificar que el inicio de sesión fue exitoso.
-        ####mañanaborrar esto esta mal####
+    def test_login_exitoso(self,driver):
+        login_page = LoginPage(self.driver)
+        login_page.navigate_to_login_page()
+        login_page.enter_username()
+        login_page.enter_password()
+        login_page.click_login_button()
+
+
+
+
+
